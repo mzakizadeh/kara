9d1b7e77eb617ef37c8822bb97e18cd51de5e51b
Mahdi Zakizadeh <mahdizakizadeh.me@gmail.com>
2025-07-13 21:28:21 +0330

    fix(readthedoc): install dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ release = "0.2.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.todo", "sphinx_rtd_theme"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.todo"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
