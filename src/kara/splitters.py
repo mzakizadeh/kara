f1249b8d00e513d68cc85bd3c022516e56256d22
Mahdi Zakizadeh <mzakizadeh.me@gmail.com>
2025-07-14 23:29:25 +0330

    docs: fix default chunk size

--- src/kara/splitters.py
+++ src/kara/splitters.py
@@ -97,7 +97,7 @@ class RecursiveCharacterChunker(BaseDocumentChunker):
 
         Args:
             separators: List of separators to try, in order of preference
-            chunk_size: Maximum chunk size in characters
+            chunk_size: Maximum chunk size in characters. Defaults to 4000
             overlap: Overlap between chunks (not implemented yet)
             keep_separator: Whether to keep separators in the result
         """
